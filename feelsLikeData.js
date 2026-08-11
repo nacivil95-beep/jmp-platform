91ff1caa3b3d772ec210750445f7e7e371657f1f
jinhong <nacivil95@gmail.com>
2026-08-11 09:40:02 +0900

    체감온도 자동 갱신 2026-08-11 09:40

--- feelsLikeData.js
+++ feelsLikeData.js
@@ -3,7 +3,7 @@
  * 생성 스크립트: fetch_feels_like.py (5분마다 자동 갱신)
  */
 let FEELS_LIKE_DATA = {
-  "feelsLike": 28.6,
-  "updatedAt": "2026-08-11 09:35:02",
+  "feelsLike": 28.5,
+  "updatedAt": "2026-08-11 09:40:02",
   "source": "sensolink (스마트 체감온도계)"
 };
