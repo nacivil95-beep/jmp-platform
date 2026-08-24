c58ecfd94fff0776823acdecfd0ff3294967f0d4
jinhong <nacivil95@gmail.com>
2026-08-24 10:01:54 +0900

    ?묒뾽?쇰낫/?덉쭏?쒗뿕/?덉쟾蹂닿굔/怨듭??ы빆/寃뚯떆???먮룞 ?낅뜲?댄듃 2026-08-24 10:01

--- notices.js
+++ notices.js
@@ -15,7 +15,7 @@
  *   pinned     : true 면 항상 상단 고정
  *
  * 원본: C:\Users\jinhong\Desktop\jmp-platform\데이터관리\알림창\공지사항.xlsx
- * 생성 시각: 2026-08-24 09:03:26
+ * 생성 시각: 2026-08-24 10:01:53
  */
 let SITE_NOTICES = [
    {
